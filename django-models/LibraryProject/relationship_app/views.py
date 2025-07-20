554d3458ff3683b4f08591e056b3031a95a757dd
polymathuniversata <polymathuniversata@outlook.com>
2025-07-21 00:21:16 +0300

    Fix: Use correct DetailView import and minimal list_books.html for Django checks

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import DetailView, ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
